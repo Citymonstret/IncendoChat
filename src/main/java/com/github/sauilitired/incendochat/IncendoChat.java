a5dfdfdae66dfc570b9312000733f0e551678d97
Alexander Söderberg <sauilitired@gmail.com>
2020-03-24 11:40:21 +0100

    Initial commit

---
+++ src/main/java/com/github/sauilitired/incendochat/IncendoChat.java
@@ -0,0 +1,15 @@
+package com.github.sauilitired.incendochat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IncendoChat extends JavaPlugin {
+
+    @Override public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
